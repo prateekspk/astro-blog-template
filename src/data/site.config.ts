fde7863f3d686a986ac985445e7b006bf97a1c67
Prateek S <prateeks@Prateeks-MacBook-Pro.local>
2025-06-22 19:11:01 +0530

    Added legal pages

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,7 +11,7 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
+	author: 'Author', // Site author
 	title: 'Astro Theme OpenBlog', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
 	lang: 'en-GB',
